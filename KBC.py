e6b4a8c15fe314bfe3ac9ee580069ebbca468a0a
Sneha Gupta <128354295+SNeha123o@users.noreply.github.com>
2023-06-21 12:00:14 +0530

    Add files via upload

---
+++ KBC.py
@@ -0,0 +1,162 @@
+print("Welcome to our sasta KBC,I hope you will play well but sorry amount is fake 😂 ")
+
+Questions=[1,2,3,4,5,6,7,8,9,10]
+Answers=["D","A","A","B","D","C","B","A","C","B"]
+Amount=[00]
+
+i=1
+while(i<=10):
+ print("Question",i,":What's a web browser?")
+ print(''' A.A kind of spider
+ B.A computer that stores WWW files
+ C.A person who likes to look at websites
+ D.A software program that allows you to access sites on the World Wide Web''')
+ choose=input("Enter you option: ")
+ if (choose==Answers[0]):
+    print("Correct,Hurrey🥳 you won 5000/-")
+    Amount.append(5000)
+    i=i+1
+ else: 
+    print("Game over") 
+    print("Prize=",5000*(len(Amount)-1)) 
+    break 
+    
+
+ print("Question",i,":http://www.indiabix.com - is an example of what?")
+ print(''' A.A URL
+ B.An access code
+ C.A directory
+ D.A server''')
+ choose=input("Enter you option: ")
+ if (choose==Answers[1]):
+    print("Correct,Hurrey🥳 you won 5000/-")
+    Amount.append(10000)
+    i=i+1
+ else: 
+    print("Game over")  
+    print("Prize=",5000*(len(Amount)-1)) 
+    break 
+
+
+ print("Question",i,":The International Literacy Day is observed on which day?")
+ print(''' A.Sep 8
+ B.Nov 28
+ C.May 2
+ D.Sep 22''')
+ choose=input("Enter you option: ")
+ if (choose==Answers[2]):
+    print("Correct,Hurrey🥳you won 5000/-")
+    Amount.append(15000)
+    i=i+1
+ else: 
+    print("Game over")   
+    print("Prize=",5000*(len(Amount)-1)) 
+    break 
+
+ print("Question",i,":What do you call a computer on a network that requests files from another computer?")
+ print(''' A.A host
+ B.A client
+ C.A router
+ D.A web server''')
+ choose=input("Enter you option: ")
+ if (choose==Answers[3]):
+    print("Correct,Hurrey🥳 you won 5000/-")
+    Amount.append(20000)
+    i=i+1
+ else: 
+    print("Game over")   
+    print("Prize=",5000*(len(Amount)-1)) 
+    break 
+
+ print("Question",i,":The main computer that stores the files that can be sent to computers that are networked together is what?")
+ print(''' A.Clip art
+ B.Mother board
+ C.Peripheral
+ D.File server''')
+ choose=input("Enter you option: ")
+ if (choose==Answers[4]):
+    print("Correct,Hurrey🥳 you won 5000/-")
+    Amount.append(25000)
+    i=i+1
+ else: 
+    print("Game over")   
+    print("Prize=",5000*(len(Amount)-1)) 
+    break 
+
+ print("Question",i,":Which is not an Internet protocol?")
+ print(''' A.HTTP
+ B.FTP
+ C.STP
+ D.IP''')
+ choose=input("Enter you option: ")
+ if (choose==Answers[5]):
+    print("Correct,Hurrey🥳 you won 5000/-")
+    Amount.append(30000)
+    i=i+1
+ else: 
+    print("Game over")   
+    print("Prize=",5000*(len(Amount)-1)) 
+    break 
+
+ print("Question",i,":Another name for a computer chip is what?")
+ print(''' A.Execute
+ B.Micro chip
+ C.Microprocessor
+ D.Select''')
+ choose=input("Enter you option: ")
+ if (choose==Answers[6]):
+    print("Correct,Hurrey🥳 you won 5000/-")
+    Amount.append(35000)
+    i=i+1
+ else: 
+    print("Game over")   
+    print("Prize=",5000*(len(Amount)-1)) 
+    break 
+
+ print("Question",i,":'www' stands for what?")
+ print(''' A.World Wide Web
+ B.World Wide Wares
+ C.World Wide Wait
+ D.World Wide War''')
+ choose=input("Enter you option: ")
+ if (choose==Answers[7]):
+    print("Correct,Hurrey🥳 you won 5000/-")
+    Amount.append(40000)
+    i=i+1
+ else: 
+    print("Game over")   
+    print("Prize=",5000*(len(Amount)-1)) 
+    break 
+
+ print("Question",i,": Which of the following is not a programming language?")
+ print(''' A.Basic
+ B.Java
+ C.Turing
+ D.C++''')
+ choose=input("Enter you option: ")
+ if (choose==Answers[8]):
+    print("Correct,Hurrey🥳 you won 5000/-")
+    Amount.append(450000)
+    i=i+1
+ else: 
+    print("Game over")   
+    print("Prize=",5000*(len(Amount)-1)) 
+    break 
+
+ print("Question",i,":ROM stands for what?")
+ print(''' A. Royal Ontario Museum
+ B.Read Only Memory
+ C.Ready on Memory
+ D.Real Obsolete Memory''')
+ choose=input("Enter you option: ")
+ if (choose==Answers[9]):
+    print("Correct,Hurrey🥳 you won 5000/-")
+    Amount.append(50000)
+    
+    print("Congratutions! 🥳 you won 50,000/-")
+    i=i+1
+ else: 
+    print("Game over")   
+    print("Prize=",5000*(len(Amount)-1)) 
+     
+    
